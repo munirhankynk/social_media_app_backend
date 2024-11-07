32847a78d5873afd22ea6d1001b3aa4eb06422f9
munirhankynk <munirhankynk@gmail.com>
2024-11-07 21:12:17 +0300

    Initial commit

---
+++ Social_Media_App/src/test/java/com/backendCase/Social_Media_App/SocialMediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendCase.Social_Media_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
